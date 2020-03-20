ff7616b2a944b63804ad1caeb94661ad2b6f329d
DisSun <dissun1987@163.com>
2020-03-20 17:16:55 +0800

    完成聊天界面的完整布局，以及打赏界面的布局设计

--- DisSunChat.UI/JsCommon/BoxScript3.js
+++ DisSunChat.UI/JsCommon/BoxScript3.js
@@ -1,1 +1,16 @@
-﻿
+﻿var PraiseEvent = function () {
+    $(".praiseDiv").show();
+    $(".contentDiv").addClass("blur");
+    $(".keyboardDiv").addClass("blur");
+}
+
+var CloseEvent = function () {
+
+    $(".praiseDiv").hide();
+    $(".contentDiv").removeClass("blur");
+    $(".keyboardDiv").removeClass("blur");
+}
+
+var SendMsgEvent = function () {
+    alert("别点了，还没做好呢！");
+}
